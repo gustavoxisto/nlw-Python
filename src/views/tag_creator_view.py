dcad5d31d65b0c4429fec3a8b3296deef3aa608c
Gustavo Xisto Notebook <curveloxisto@gmail.com>
2024-02-08 08:08:02 -0300

    Feat: tratamento de erros

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -7,10 +8,12 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body["product_code"]
 
         # Logica de regra de negocio
-        print('Estou na minha view')
+        formatted_response = tag_creator_controller.create(product_code)
         # Retorno http
-        return HttpResponse(status_code=200, body={"Hello": "World"})
+        return HttpResponse(status_code=200, body=formatted_response)
